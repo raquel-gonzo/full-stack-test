e13f629fed4d0787c1fa6285544e87eee8c7cc06
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-12 20:34:10 -0500

    now able to make cross-origin requests. front-end is connected to express.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 require('./server/routes/person.routes')(app);
 
