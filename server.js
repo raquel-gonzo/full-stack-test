163924a663ef0679df26984cc73a0d3c1e38faa8
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-12 20:01:21 -0500

    back end is set up. completed controllers and routes files.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-    
+
+require('./server/routes/person.routes')(app);
+
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
